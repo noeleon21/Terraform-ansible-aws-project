a6c372ad3befd597fe44dd8024b38232ee7617e6
noeleon21 <noelgens@gmail.com>
2025-12-31 20:08:58 +1100

    changed playbook file

--- ssm.tf
+++ ssm.tf
@@ -18,7 +18,7 @@ resource "aws_ssm_association" "ansible_association" {
       path = "https://s3.amazonaws.com/${aws_s3_bucket.ansible.bucket}/playbook.zip"
     })
 
-    PlaybookFile        = "inventory.yml"
+    PlaybookFile        = "playbook/inventory.yml"
     InstallDependencies = "True"
   }
 }
