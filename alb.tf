ceb52b8736699a54300e3ad8259481e824243ff0
noeleon21 <noelgens@gmail.com>
2026-03-08 15:49:08 +1100

    disabled deletion protection

--- alb.tf
+++ alb.tf
@@ -3,6 +3,7 @@ module "alb" {
   name    = "my-alb234432"
   vpc_id  = module.vpc.vpc_id
   subnets = module.vpc.public_subnets
+  enable_deletion_protection = false
 
   security_group_ingress_rules = {
     http_from_cloudfront = {
